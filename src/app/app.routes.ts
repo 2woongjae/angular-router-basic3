51fe98e39ed480970a7b87785109482b800d7856
Mark Lee <mark@protopie.io>
2017-12-09 00:43:00 +0900

    develper => developer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { DevelperComponent } from './develper/develper.component';
+import { DeveloperComponent } from './developer/developer.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'developer', component: DevelperComponent}
+    {path: 'developer', component: DeveloperComponent}
 ];
 
 export default RouterModule.forRoot(routes);
